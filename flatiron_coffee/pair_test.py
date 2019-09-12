c13ab9f2e04f1916bce40448577ab79d39276dd2
Dan F-M <foreman.mackey@gmail.com>
2019-09-12 08:45:58 -0400

    fixing bug in test

--- flatiron_coffee/pair_test.py
+++ flatiron_coffee/pair_test.py
@@ -10,5 +10,5 @@ def test_no_matches():
         (emails[0], emails[2]),
         (emails[1], emails[2]),
     }
-    graph = set(find_pairs(emails, previous_pairs))
+    graph = set(find_pairs(emails, previous_pairs)[0])
     assert len(graph & previous_pairs) == 0
