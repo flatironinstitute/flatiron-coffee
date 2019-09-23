236cc2d85184e579a930545c682ef65b60e17370
Dan F-M <foreman.mackey@gmail.com>
2019-09-23 07:20:00 -0400

    fixing cli

--- execute.py
+++ execute.py
@@ -5,7 +5,7 @@ import argparse
 from flatiron_coffee import find_matches
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--send", help="send the emails")
+parser.add_argument("--send", action="store_true", help="send the emails")
 args = parser.parse_args()
 
 dry_run = True
